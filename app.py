d87cd795b89b26501d938e37a69a3205f7a5decc
luchaf <26599038+luchaf@users.noreply.github.com>
2023-09-22 23:33:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import datetime
 import streamlit as st
+import pandas as pd
 
 title = st.text_input('copy paste game results from whatsapp chat here:')
 #st.write('Game results', title)
 
 d = st.date_input("Day to document the results:", datetime.datetime.now())
 st.write('Day to document:', d)
+
+df = pd.read_csv(st.secrets["public_gsheets_url"])
+print(df)
+df
+df
+print(len(df))
