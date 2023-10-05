8f909621d9969e9226c24a1b473d47a6b5a61973
luchaf <26599038+luchaf@users.noreply.github.com>
2023-10-05 23:20:52 +0200

    Update 2_Overanalysis_Oasis.py

--- pages/2_Overanalysis_Oasis.py
+++ pages/2_Overanalysis_Oasis.py
@@ -14,6 +14,7 @@ from datetime import date
 st.title('Overanalysis Oasis')
 
 # Load data from database
+df = pd.DataFrame()
 df_sheet = pd.read_csv(st.secrets["public_gsheets_url"])
 df_sheet["date"] = df_sheet["date"].astype(str)
 list_of_available_dates = list(set(df_sheet["date"].tolist()))
@@ -47,10 +48,11 @@ with settings_tab:
                 st.stop()
 
             sorted_dates = sorted(match_dates)
-            dates_str = ", ".join([d.strftime('%Y-%m-%d') for d in sorted_dates])
+            dates_str = ", ".join([d for d in sorted_dates])
             st.info(f"Available match dates: {dates_str}", icon="ℹ️")
 
-            match_day = st.date_input('Select a specific match day', value=date.today())
+            match_day = st.date_input('Select a specific match day')
+            match_day = match_day.strftime('%Y%m%d')
             if match_day in match_dates:
                 st.write(f"You've selected {match_day} as the match day of interest!")
                 df = df_tmp[df_tmp["date"] == match_day].copy()
@@ -68,7 +70,9 @@ with settings_tab:
 
         elif date_range:
             start_date = st.date_input('Start Date', value=date.today())
+            start_date = start_date.strftime('%Y%m%d')
             end_date = st.date_input('End Date', value=date.today())
+            end_date = end_date.strftime('%Y%m%d')
 
             if start_date > end_date:
                 st.warning('Start date should be before or the same as end date.')
@@ -76,31 +80,11 @@ with settings_tab:
                 matches_in_range = [d for d in match_dates if start_date <= d <= end_date]
                 if matches_in_range:
                     sorted_dates = sorted(matches_in_range)
-                    dates_str = ", ".join([d.strftime('%Y-%m-%d') for d in sorted_dates])
+                    dates_str = ", ".join([d for d in sorted_dates])
                     st.info(f"Matches between {start_date} and {end_date}: {dates_str}", icon="ℹ️")
                     df = df_tmp[((df_tmp["date"] >= start_date) & (df_tmp["date"] <= end_date))].copy()
                 else:
                     st.warning(f"No matches between {start_date} and {end_date}.")
-
-        if not df.empty:
-            # Dominance Scores
-            df = df.reset_index(drop=True).copy()
-            df = df.reset_index()
-            # Derive player and combination stats
-            combination_stats = calculate_combination_stats(df)
-            df = get_name_opponent_name_df(df)
-
-            # Calculate individual stats
-            players_stats = df.groupby('Name').agg({'Wins': 'sum', 'Player Score': 'sum'}).rename(
-                columns={'Player Score': 'Total Score'}).sort_values("Wins", ascending=False)
-
-            # Derive results
-            results = derive_results(df)
-
-            # Calculate win and loss streaks
-            # streaks = calculate_streaks(results)
-            streaks = get_name_streaks_df(df)
-            streaks = streaks.sort_values(["longest_win_streak", "longest_loss_streak"], ascending=False)
 
     with st.expander("Adjust aesthetics"):
         col_friede, col_simon, col_lucas = st.columns(3)
@@ -117,55 +101,77 @@ with settings_tab:
         }
         title_color = 'black'
 
-with basic_metrics_tab:
-    Number_of_Wins_tab, Total_Points_Scored_tab = st.tabs(["# Wins", "# Points Scored"])
-    with Number_of_Wins_tab:
-        st.info(f"How many wins did each player collect...", icon="❓")
-        total_wins_tab, face_to_face_wins_tab = st.tabs(["Total", "Face-to-Face-Feud"])
-        with total_wins_tab:
-            st.info(f"...in total: static or over time", icon="❓")
-            wins_all_time_tab, wins_over_time_tab = st.tabs(["static", "over time"])
-            with wins_all_time_tab:
-                plot_bars(players_stats, title_color, player_colors, "Wins")
-            with wins_over_time_tab:
-                cumulative_wins_over_time(df, player_colors, title_color, "Wins")
-        with face_to_face_wins_tab:
-            st.info(f"...against specific opponents: static or over time", icon="❓")
-            wins_face_to_face_all_time_tab, wins_face_to_face_over_time_tab = st.tabs(["static", "over time"])
-            with wins_face_to_face_all_time_tab:
-                plot_player_combo_graph(combination_stats, player_colors, "Wins")
-            with wins_face_to_face_over_time_tab:
-                entities_face_to_face_over_time(df, player_colors, title_color, "Wins")
+    if df.empty:
+        st.warning('Please select at least one valid matchday.')
+    else:
+        # Dominance Scores
+        df = df.reset_index(drop=True).copy()
+        df = df.reset_index()
+        # Derive player and combination stats
+        combination_stats = calculate_combination_stats(df)
+        df = get_name_opponent_name_df(df)
 
-    with Total_Points_Scored_tab:
-        st.info(f"How many points did each player score...", icon="❓")
-        total_score_tab, face_to_face_score_tab = st.tabs(["Total", "Face-to-Face-Feud"])
-        with total_score_tab:
-            st.info(f"...in total: static or over time", icon="❓")
-            scores_all_time_tab, scores_over_time_tab = st.tabs(["static", "over time"])
-            with scores_all_time_tab:
-                plot_bars(players_stats, title_color, player_colors, "Total Score")
-            with scores_over_time_tab:
-                cumulative_wins_over_time(df, player_colors, title_color, "Total Score")
-        with face_to_face_score_tab:
-            st.info(f"...against specific opponents: static or over time", icon="❓")
-            scores_face_to_face_all_time_tab, scores_face_to_face_over_time_tab = st.tabs(["static", "over time"])
-            with scores_face_to_face_all_time_tab:
-                plot_player_combo_graph(combination_stats, player_colors, "Total Score")
-            with scores_face_to_face_over_time_tab:
-                entities_face_to_face_over_time(df, player_colors, title_color, "Player Score")
+        # Calculate individual stats
+        players_stats = df.groupby('Name').agg({'Wins': 'sum', 'Player Score': 'sum'}).rename(
+            columns={'Player Score': 'Total Score'}).sort_values("Wins", ascending=False)
 
-with intermediate_metrics_tab:
-    (streaks_tab,
-     trends_tab) = st.tabs(["Streaks",
-                            "Trends"])
-    with streaks_tab:
-        st.info(f"Longest number of consecutive wins or losses by each player", icon="❓")
-        static_streaks, over_time_streaks = st.tabs(["Static", "Over Time"])
-        with static_streaks:
-            graph_win_and_loss_streaks(streaks, title_color)
-        with over_time_streaks:
-            wins_and_losses_over_time_plot(results, player_colors, title_color)
-    with trends_tab:
-        win_loss_trends_plot(results, player_colors, title_color)
+        # Derive results
+        results = derive_results(df)
+
+        # Calculate win and loss streaks
+        # streaks = calculate_streaks(results)
+        streaks = get_name_streaks_df(df)
+        streaks = streaks.sort_values(["longest_win_streak", "longest_loss_streak"], ascending=False)
+
+        with basic_metrics_tab:
+            Number_of_Wins_tab, Total_Points_Scored_tab = st.tabs(["# Wins", "# Points Scored"])
+            with Number_of_Wins_tab:
+                st.info(f"How many wins did each player collect...", icon="❓")
+                total_wins_tab, face_to_face_wins_tab = st.tabs(["Total", "Face-to-Face-Feud"])
+                with total_wins_tab:
+                    st.info(f"...in total: static or over time", icon="❓")
+                    wins_all_time_tab, wins_over_time_tab = st.tabs(["static", "over time"])
+                    with wins_all_time_tab:
+                        plot_bars(players_stats, title_color, player_colors, "Wins")
+                    with wins_over_time_tab:
+                        cumulative_wins_over_time(df, player_colors, title_color, "Wins")
+                with face_to_face_wins_tab:
+                    st.info(f"...against specific opponents: static or over time", icon="❓")
+                    wins_face_to_face_all_time_tab, wins_face_to_face_over_time_tab = st.tabs(["static", "over time"])
+                    with wins_face_to_face_all_time_tab:
+                        plot_player_combo_graph(combination_stats, player_colors, "Wins")
+                    with wins_face_to_face_over_time_tab:
+                        entities_face_to_face_over_time(df, player_colors, title_color, "Wins")
+
+            with Total_Points_Scored_tab:
+                st.info(f"How many points did each player score...", icon="❓")
+                total_score_tab, face_to_face_score_tab = st.tabs(["Total", "Face-to-Face-Feud"])
+                with total_score_tab:
+                    st.info(f"...in total: static or over time", icon="❓")
+                    scores_all_time_tab, scores_over_time_tab = st.tabs(["static", "over time"])
+                    with scores_all_time_tab:
+                        plot_bars(players_stats, title_color, player_colors, "Total Score")
+                    with scores_over_time_tab:
+                        cumulative_wins_over_time(df, player_colors, title_color, "Total Score")
+                with face_to_face_score_tab:
+                    st.info(f"...against specific opponents: static or over time", icon="❓")
+                    scores_face_to_face_all_time_tab, scores_face_to_face_over_time_tab = st.tabs(["static", "over time"])
+                    with scores_face_to_face_all_time_tab:
+                        plot_player_combo_graph(combination_stats, player_colors, "Total Score")
+                    with scores_face_to_face_over_time_tab:
+                        entities_face_to_face_over_time(df, player_colors, title_color, "Player Score")
+
+        with intermediate_metrics_tab:
+            (streaks_tab,
+             trends_tab) = st.tabs(["Streaks",
+                                    "Trends"])
+            with streaks_tab:
+                st.info(f"Longest number of consecutive wins or losses by each player", icon="❓")
+                static_streaks, over_time_streaks = st.tabs(["Static", "Over Time"])
+                with static_streaks:
+                    graph_win_and_loss_streaks(streaks, title_color)
+                with over_time_streaks:
+                    wins_and_losses_over_time_plot(results, player_colors, title_color)
+            with trends_tab:
+                win_loss_trends_plot(results, player_colors, title_color)
 
