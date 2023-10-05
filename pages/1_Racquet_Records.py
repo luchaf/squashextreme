b3b1524f9c367d1ba6d81028b1fb37dd4f7d2619
luchaf <26599038+luchaf@users.noreply.github.com>
2023-10-05 22:25:36 +0200

    added new files

---
+++ pages/1_Racquet_Records.py
@@ -0,0 +1,87 @@
+import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
+
+def reset_session_state():
+    """Helper function to reset session state."""
+    st.session_state['player1_name'] = ''
+    st.session_state['player1_score'] = None
+    st.session_state['player2_name'] = ''
+    st.session_state['player2_score'] = None
+    st.session_state['matchday_input'] = None
+    st.session_state['show_confirm'] = False
+    st.session_state['data_written'] = False
+    
+def display_enter_match_results():
+    # Initialize session state values if not already set
+    if 'player1_name' not in st.session_state:
+        st.session_state['player1_name'] = ''
+    if 'player1_score' not in st.session_state:
+        st.session_state['player1_score'] = None
+    if 'player2_name' not in st.session_state:
+        st.session_state['player2_name'] = ''
+    if 'player2_score' not in st.session_state:
+        st.session_state['player2_score'] = None
+    if 'matchday_input' not in st.session_state:
+        st.session_state['matchday_input'] = None
+    if 'show_confirm' not in st.session_state:
+        st.session_state['show_confirm'] = False
+    if 'data_written' not in st.session_state:
+        st.session_state['data_written'] = False
+
+    if st.session_state['data_written']:
+        st.success("Successfully wrote match result to database. Do you want to enter a new match result?")
+        if st.button("Enter New Match Result"):
+            reset_session_state()
+            st.experimental_rerun()
+
+    else:
+        st.title("Racquet Records: Document your match results")
+
+        st.write("Log your praiseworthy or pitiful match results here:")
+    
+        # Use session state for values
+        st.session_state['player1_name'] = st.text_input("Player 1 Name", st.session_state['player1_name'])
+    
+        if st.session_state['player1_name']:
+            st.session_state['player1_score'] = st.number_input("Player 1 Score", min_value=0, value=st.session_state['player1_score'], step=1)
+            if st.session_state['player1_score'] or st.session_state['player1_score'] == 0:
+                st.session_state['player2_name'] = st.text_input("Player 2 Name", st.session_state['player2_name'])
+                if st.session_state['player2_name']:
+                    st.session_state['player2_score'] = st.number_input("Player 2 Score", min_value=0, value=st.session_state['player2_score'], step=1)
+                    if st.session_state['player2_score'] or st.session_state['player2_score'] == 0:
+                        st.session_state['matchday_input'] = st.date_input("Matchday", st.session_state['matchday_input'] if st.session_state['matchday_input'] else None)
+    
+        if st.session_state['matchday_input'] and (st.session_state['player1_name'] or st.session_state['player2_name']):
+            if st.button("Preview"):
+                st.subheader("Confirm the following match result:")
+                st.write(f"{st.session_state['player1_name']}: {st.session_state['player1_score']} - {st.session_state['player2_name']}: {st.session_state['player2_score']} on {st.session_state['matchday_input']}")
+                st.session_state['show_confirm'] = True
+    
+        if st.session_state['show_confirm']:
+            if st.button("Confirm"):
+                spreadsheetId = st.secrets["public_gsheets_id"]
+                scope = ['https://www.googleapis.com/auth/spreadsheets']
+                google_creds_dict = dict(st.secrets["google_creds"])
+                # Create credentials from the dictionary
+                credentials = Credentials.from_service_account_info(google_creds_dict, scopes=scope)
+                client = gspread.authorize(credentials)
+                spreadsheet = client.open_by_key(spreadsheetId)
+                worksheet = spreadsheet.sheet1
+                match_result = f"{st.session_state['player1_name']} - {st.session_state['player2_name']} {st.session_state['player1_score']}:{st.session_state['player2_score']}"
+                game_date = int(st.session_state['matchday_input'].strftime('%Y%m%d'))
+                # Append the new game and its date to the worksheet
+                worksheet.append_row([match_result, game_date])
+                # Clear the inputs and flag data as written
+                st.session_state['data_written'] = True               
+                # Clear the inputs
+                st.session_state['player1_name'] = ''
+                st.session_state['player1_score'] = None
+                st.session_state['player2_name'] = ''
+                st.session_state['player2_score'] = None
+                st.session_state['matchday_input'] = None
+                st.session_state['show_confirm'] = False
+    
+                st.experimental_rerun()  # This will rerun the script and update the UI with cleared inputs
+
+display_enter_match_results()
---
+++ pages/2_Overanalysis_Oasis.py
@@ -0,0 +1,171 @@
+from utils import (extract_data_from_games, get_name_opponent_name_df, get_name_streaks_df,
+                   calculate_combination_stats,
+                   derive_results,
+                   win_loss_trends_plot,
+                   wins_and_losses_over_time_plot,
+                   graph_win_and_loss_streaks,
+                   plot_player_combo_graph,
+                   plot_bars, cumulative_wins_over_time, entities_face_to_face_over_time)
+import streamlit as st
+import pandas as pd
+from datetime import date
+
+# Streamlit app
+st.title('Overanalysis Oasis')
+
+# Load data from database
+df_sheet = pd.read_csv(st.secrets["public_gsheets_url"])
+df_sheet["date"] = df_sheet["date"].astype(str)
+list_of_available_dates = list(set(df_sheet["date"].tolist()))
+df_sheet['parsed_sheet_df'] = df_sheet.apply(lambda x: extract_data_from_games(x["games"], x["date"]), axis=1)
+df_tmp = pd.DataFrame()
+for _, row in df_sheet.iterrows():
+    df_tmp = pd.concat([df_tmp, row["parsed_sheet_df"]])
+
+(
+    settings_tab,
+    basic_metrics_tab,
+    intermediate_metrics_tab,
+) = st.tabs(["Settings :gear:",
+             "Basic Metrics :star:",
+             "Intermediate Metrics :exploding_head:",
+             ])
+
+with settings_tab:
+    with (st.expander("Lob your preferred time period into the analysis court.")):
+        # Sample data: list of dates when matches occurred
+        match_dates = list(set(df_sheet["date"].tolist()))
+
+        all_match_days = st.checkbox('All-court days', value=True)
+        specific_match_day = st.checkbox('That one day on the court')
+        date_range = st.checkbox('Court calendar slice')
+
+        if specific_match_day:
+            if all_match_days or date_range:
+                # Warn the user if they select multiple options
+                st.warning('Please select only one option.')
+                st.stop()
+
+            sorted_dates = sorted(match_dates)
+            dates_str = ", ".join([d.strftime('%Y-%m-%d') for d in sorted_dates])
+            st.info(f"Available match dates: {dates_str}", icon="ℹ️")
+
+            match_day = st.date_input('Select a specific match day', value=date.today())
+            if match_day in match_dates:
+                st.write(f"You've selected {match_day} as the match day of interest!")
+                df = df_tmp[df_tmp["date"] == match_day].copy()
+            else:
+                st.warning(f"No matches on {match_day}. Please refer to the list for match days.")
+
+        elif all_match_days:
+            if date_range:
+                # Warn the user if they select multiple options
+                st.warning('Please select only one option.')
+                st.stop()
+
+            st.write("You've selected analysis for all match days!")
+            df = df_tmp.copy()
+
+        elif date_range:
+            start_date = st.date_input('Start Date', value=date.today())
+            end_date = st.date_input('End Date', value=date.today())
+
+            if start_date > end_date:
+                st.warning('Start date should be before or the same as end date.')
+            else:
+                matches_in_range = [d for d in match_dates if start_date <= d <= end_date]
+                if matches_in_range:
+                    sorted_dates = sorted(matches_in_range)
+                    dates_str = ", ".join([d.strftime('%Y-%m-%d') for d in sorted_dates])
+                    st.info(f"Matches between {start_date} and {end_date}: {dates_str}", icon="ℹ️")
+                    df = df_tmp[((df_tmp["date"] >= start_date) & (df_tmp["date"] <= end_date))].copy()
+                else:
+                    st.warning(f"No matches between {start_date} and {end_date}.")
+
+        if not df.empty:
+            # Dominance Scores
+            df = df.reset_index(drop=True).copy()
+            df = df.reset_index()
+            # Derive player and combination stats
+            combination_stats = calculate_combination_stats(df)
+            df = get_name_opponent_name_df(df)
+
+            # Calculate individual stats
+            players_stats = df.groupby('Name').agg({'Wins': 'sum', 'Player Score': 'sum'}).rename(
+                columns={'Player Score': 'Total Score'}).sort_values("Wins", ascending=False)
+
+            # Derive results
+            results = derive_results(df)
+
+            # Calculate win and loss streaks
+            # streaks = calculate_streaks(results)
+            streaks = get_name_streaks_df(df)
+            streaks = streaks.sort_values(["longest_win_streak", "longest_loss_streak"], ascending=False)
+
+    with st.expander("Adjust aesthetics"):
+        col_friede, col_simon, col_lucas = st.columns(3)
+        with col_friede:
+            color_friedemann = st.color_picker('Friedemann', '#ffc0cb')
+        with col_simon:
+            color_simon = st.color_picker('Simon', '#004d9d')
+        with col_lucas:
+            color_lucas = st.color_picker('Lucas', '#7CFC00')
+        player_colors = {
+            'Simon': color_simon,
+            'Friedemann': color_friedemann,
+            'Lucas': color_lucas,
+        }
+        title_color = 'black'
+
+with basic_metrics_tab:
+    Number_of_Wins_tab, Total_Points_Scored_tab = st.tabs(["# Wins", "# Points Scored"])
+    with Number_of_Wins_tab:
+        st.info(f"How many wins did each player collect...", icon="❓")
+        total_wins_tab, face_to_face_wins_tab = st.tabs(["Total", "Face-to-Face-Feud"])
+        with total_wins_tab:
+            st.info(f"...in total: static or over time", icon="❓")
+            wins_all_time_tab, wins_over_time_tab = st.tabs(["static", "over time"])
+            with wins_all_time_tab:
+                plot_bars(players_stats, title_color, player_colors, "Wins")
+            with wins_over_time_tab:
+                cumulative_wins_over_time(df, player_colors, title_color, "Wins")
+        with face_to_face_wins_tab:
+            st.info(f"...against specific opponents: static or over time", icon="❓")
+            wins_face_to_face_all_time_tab, wins_face_to_face_over_time_tab = st.tabs(["static", "over time"])
+            with wins_face_to_face_all_time_tab:
+                plot_player_combo_graph(combination_stats, player_colors, "Wins")
+            with wins_face_to_face_over_time_tab:
+                entities_face_to_face_over_time(df, player_colors, title_color, "Wins")
+
+    with Total_Points_Scored_tab:
+        st.info(f"How many points did each player score...", icon="❓")
+        total_score_tab, face_to_face_score_tab = st.tabs(["Total", "Face-to-Face-Feud"])
+        with total_score_tab:
+            st.info(f"...in total: static or over time", icon="❓")
+            scores_all_time_tab, scores_over_time_tab = st.tabs(["static", "over time"])
+            with scores_all_time_tab:
+                plot_bars(players_stats, title_color, player_colors, "Total Score")
+            with scores_over_time_tab:
+                cumulative_wins_over_time(df, player_colors, title_color, "Total Score")
+        with face_to_face_score_tab:
+            st.info(f"...against specific opponents: static or over time", icon="❓")
+            scores_face_to_face_all_time_tab, scores_face_to_face_over_time_tab = st.tabs(["static", "over time"])
+            with scores_face_to_face_all_time_tab:
+                plot_player_combo_graph(combination_stats, player_colors, "Total Score")
+            with scores_face_to_face_over_time_tab:
+                entities_face_to_face_over_time(df, player_colors, title_color, "Player Score")
+
+with intermediate_metrics_tab:
+    (streaks_tab,
+     trends_tab) = st.tabs(["Streaks",
+                            "Trends"])
+    with streaks_tab:
+        st.info(f"Longest number of consecutive wins or losses by each player", icon="❓")
+        static_streaks, over_time_streaks = st.tabs(["Static", "Over Time"])
+        with static_streaks:
+            graph_win_and_loss_streaks(streaks, title_color)
+        with over_time_streaks:
+            wins_and_losses_over_time_plot(results, player_colors, title_color)
+    with trends_tab:
+        win_loss_trends_plot(results, player_colors, title_color)
+
