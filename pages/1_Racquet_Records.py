958e4790c0877eb918368e4796d44c9d7bd613c2
Luc <lucashafner@gmail.com>
2023-12-02 12:45:59 +0100

    drive

--- pages/1_Racquet_Records.py
+++ pages/1_Racquet_Records.py
@@ -3,6 +3,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
 import tempfile
 import os
 from utils import (extract_data_from_games, get_name_opponent_name_df, get_name_streaks_df,
@@ -17,12 +18,6 @@ import pandas as pd
 from datetime import date
 from pathlib import Path
 from openai import OpenAI
-
-
-
-
-
-
 
 
 (
