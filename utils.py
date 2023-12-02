cb43e0c3944ba28b33fb1de6110b51bb7347908f
Luc <lucashafner@gmail.com>
2023-12-02 13:43:47 +0100

    plotly

--- utils.py
+++ utils.py
@@ -408,7 +408,7 @@ def plot_bars(df2, title_color, player_colors, entity):
     plt.tight_layout()
     st.pyplot(fig, transparent=True)
 
-def plot_bars(df2, player_colors, entity):
+def plot_bars(df2, title_color, player_colors, entity):
     # Prepare data for Plotly
     data = []
     for idx, player in enumerate(df2.index):
@@ -422,27 +422,24 @@ def plot_bars(df2, player_colors, entity):
     # Create the figure with the data
     fig = go.Figure(data=data)
 
-    # Define a high-contrast color for the text, based on the player names color
-    text_color = "#FFFFFF"  # Assuming player names are in white for high contrast
-
     # Update layout for aesthetics and labels
     fig.update_layout(
-        yaxis=dict(title=entity, titlefont=dict(color=text_color)),
-        xaxis=dict(title='Players', titlefont=dict(color=text_color), tickangle=-45),
-        plot_bgcolor='rgba(0,0,0,0)', # Transparent background for the plot
-        paper_bgcolor='rgba(0,0,0,0.5)', # Semi-transparent dark background for the figure
-        font=dict(color=text_color),
+        yaxis=dict(title=entity, titlefont=dict(color=title_color)),
+        xaxis=dict(title='Players', titlefont=dict(color=title_color), tickangle=-45),
+        plot_bgcolor='rgba(0,0,0,0)',  # Transparent background for the plot
+        paper_bgcolor='rgba(0,0,0,0.5)',  # Semi-transparent dark background for the figure
+        font=dict(color=title_color),
         margin=dict(l=10, r=10, t=10, b=10),
-        bargap=0.2, # Adjust the spacing between bars
+        bargap=0.2,  # Adjust the spacing between bars
     )
 
-    # Annotations for each bar with high-contrast color
+    # Annotations for each bar with the title_color for high contrast
     for idx, player in enumerate(df2.index):
         fig.add_annotation(
             x=player,
             y=df2[entity][idx],
             text=str(int(df2[entity][idx])),
-            font=dict(color=text_color),
+            font=dict(color=title_color),
             showarrow=False,
             yshift=10
         )
