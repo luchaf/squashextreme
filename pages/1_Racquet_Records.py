19d8fbc8308334e6afd8d0112572de62c22b1c34
Luc <lucashafner@gmail.com>
2023-12-02 12:53:33 +0100

    okok

--- pages/1_Racquet_Records.py
+++ pages/1_Racquet_Records.py
@@ -262,7 +262,7 @@ with voice:
                 media_body = MediaFileUpload(temp_audio_file.name, mimetype="audio/mpeg")
                 file_metadata = {
                     "name": "generated_audio.mp3",
-                    #"parents": ["test"]
+                    "parents": ["1CjjjPV399Dbmnli7fLi2htYMIH_U8tvg"]
                 }
                 drive_service.files().create(
                     body=file_metadata,
