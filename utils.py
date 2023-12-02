b14aea95237cc673ee700fced92a8915e0db5f1e
Luc <lucashafner@gmail.com>
2023-12-02 20:36:25 +0100

    plotly

--- utils.py
+++ utils.py
@@ -293,6 +293,58 @@ def graph_win_and_loss_streaks(df1, title_color):
     plt.tight_layout()
     st.pyplot(plt, transparent=True)
 
+def graph_win_and_loss_streaks(df1, title_color):
+    # Define colors for the win and loss streaks
+    colors = {'longest_win_streak': 'green', 'longest_loss_streak': 'red'}
+
+    # Prepare data for Plotly
+    data = [
+        go.Bar(
+            name='Longest Win Streak',
+            x=df1['Name'],
+            y=df1['longest_win_streak'],
+            marker=dict(color=colors['longest_win_streak'])
+        ),
+        go.Bar(
+            name='Longest Loss Streak',
+            x=df1['Name'],
+            y=df1['longest_loss_streak'],
+            marker=dict(color=colors['longest_loss_streak'])
+        )
+    ]
+
+    # Create the figure with the data
+    fig = go.Figure(data=data)
+
+    # Update layout for aesthetics and labels
+    fig.update_layout(
+        title='Streaks for Players',
+        title_font=dict(size=16, color=title_color),
+        yaxis=dict(title='Number of Matches', titlefont=dict(size=14, color=title_color)),
+        xaxis=dict(title='Players', titlefont=dict(size=14, color=title_color)),
+        plot_bgcolor='rgba(0,0,0,0)', # Fully transparent background for the plot
+        paper_bgcolor='rgba(0,0,0,0)', # Fully transparent background for the paper
+        font=dict(color=title_color),
+        margin=dict(l=10, r=10, t=10, b=10),
+        bargap=0.2, # Adjust the spacing between bars
+        showlegend=False
+    )
+
+    # Annotations for each bar with the title_color for high contrast
+    for bar in fig.data:
+        for idx in range(len(bar.x)):
+            fig.add_annotation(
+                x=bar.x[idx],
+                y=bar.y[idx],
+                text=str(int(bar.y[idx])),
+                font=dict(color=title_color),
+                showarrow=False,
+                yshift=10 if bar.y[idx] > 0 else -10
+            )
+
+    # Streamlit Plotly display
+    st.plotly_chart(fig, use_container_width=True)
+
 
 def get_colors(players, color_map):
     return [color_map[player] for player in players]
