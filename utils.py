1424b58727ff6f507f9e4e081374f1fde1f0e123
Luc <lucashafner@gmail.com>
2023-12-02 21:04:24 +0100

    alt

--- utils.py
+++ utils.py
@@ -309,19 +309,18 @@ def graph_win_and_loss_streaks(df1, title_color):
     color_scale = alt.Scale(domain=['longest_win_streak', 'longest_loss_streak'],
                             range=['green', 'red'])
 
-    # Define base chart with common encoding
-    base = alt.Chart(df_long).encode(
+    # Create the bar chart
+    chart = alt.Chart(df_long).mark_bar().encode(
         x=alt.X('Name:N', axis=alt.Axis(title='Players', titleColor=title_color)),
         y=alt.Y('Matches:Q', axis=alt.Axis(title='Number of Matches', titleColor=title_color)),
         color=alt.Color('Streak:N', scale=color_scale),
+        column=alt.Column('Streak:N', header=alt.Header(title=None)), # Use column to create side by side bars
         tooltip=['Name', 'Streak', 'Matches']
+    ).properties(
+        width=30 # Set the width of each bar
+    ).configure_view(
+        strokeOpacity=0 # Remove the stroke between side by side bars
     )
-
-    # Create the bar chart
-    bars = base.mark_bar().properties(width=alt.Step(20))  # Control the width of each bar
-
-    # Combine the bars and text
-    chart = bars.facet(column='Streak:N')  # Facet by streak to group bars side by side
 
     # Display the chart
     st.altair_chart(chart, use_container_width=True)
